01fab99204d98ba6126ddaae7efdcea6fb25ff75
Dianella Sy <dianellabsy@csu.fullerton.edu>
2025-09-26 21:49:02 -0700

    Add Music

--- fall_2025/halloween_website/src/routes/+layout.svelte
+++ fall_2025/halloween_website/src/routes/+layout.svelte
@@ -3,56 +3,62 @@
   import { onMount } from 'svelte';
 
   let backgroundMusic: HTMLAudioElement;
-  let autoplayBlocked = false;
+  let isPlaying: boolean | null = null;
 
   onMount(() => {
     backgroundMusic = new Audio('/audio/halloween-music.mp3');
     backgroundMusic.loop = true;
-    backgroundMusic.play().catch(() => {
-      autoplayBlocked = true;
-      console.log('Autoplay blocked. User interaction required.');
-    });
   });
 
-  function playMusic() {
-    backgroundMusic.play();
-    autoplayBlocked = false;
+  function toggleMusic() {
+    if (isPlaying) {
+      backgroundMusic.pause();
+    } else {
+      backgroundMusic.play();
+    }
+    isPlaying = !isPlaying;
   }
 </script>
 
 <svelte:body />
 
 <header>
+  <div class="sound-icon" on:click={toggleMusic}>
+    {#if isPlaying === null}
+      🎵
+    {:else if isPlaying}
+      🔊
+    {:else}
+      🔈
+    {/if}
+  </div>
   <h1 class="creepy">Open Source Spooktacular Halloween</h1>
 </header>
-
-{#if autoplayBlocked}
-  <button on:click={playMusic}>Play Music</button>
-{/if}
 
 <slot />
 
 <style>
   header {
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    align-items: center;
     padding: 1rem;
     background: rgba(0,0,0,0.5);
+    position: relative;
   }
 
   header h1 {
-    font-size: 5rem;
+    font-size: 10rem;
     color: #ff7518;
+    text-align: center;
   }
 
-  button {
-    display: block;
-    margin: 1rem auto;
-    padding: 0.5rem 1rem;
-    font-size: 1rem;
-    background: var(--accent);
-    color: #fff;
-    border: none;
+  .sound-icon {
+    font-size: 2rem;
     cursor: pointer;
+    color: #ff7518;
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
   }
 </style>
